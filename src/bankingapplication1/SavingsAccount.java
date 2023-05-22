7fa6e8104ee86a9fc2279a1e6dd186ddf1f45633
NitisakKK <nitisak.kangk@kmutt.ac.th>
2023-05-22 19:31:32 +0700

    First commit

---
+++ src/bankingapplication1/SavingsAccount.java
@@ -0,0 +1,45 @@
+
+package bankingapplication1;
+
+public class SavingsAccount implements Account{
+    private int accountNumber;
+    private String accountName;
+    private double balance;
+    private final String accountType = "SavingsAccount";
+    
+    public SavingsAccount(int accountNumber, String accountName, double balance){
+        this.accountNumber = accountNumber;
+        this.accountName = accountName;
+        this.balance = balance;
+    }
+    
+    @Override
+    public int getAccountNumber(){
+        return this.accountNumber;
+    }
+    
+    public String getAccountName(){
+        return this.accountName;
+    }
+    
+    @Override
+    public void deposit(double amount){
+        this.balance = this.balance + amount;
+    }
+    
+    @Override
+    public void withdraw(double amount){
+        this.balance = this.balance - amount;
+    }
+    
+    @Override
+    public double getBalance(){
+        return this.balance;
+    }
+    
+    @Override
+    public String getAccountType(){
+        return this.accountType;
+    }
+
+}
